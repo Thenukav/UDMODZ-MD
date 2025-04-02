7b2923877b6e5ab591f5c335abe56f722a5f275d
THENUKAm <96807942+Thenukav@users.noreply.github.com>
2025-04-02 22:15:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "සෙසන් දාන්න", // session id
+SESSION_ID: process.env.SESSION_ID || "SUPUN-MD=SQ8QVKBK#SfG6DIR7TL83uDy4x2aprG4SXExf7BbEh8WF08V6sqo", // session id
 ALIVE_IMG : process.env.ALIVE_IMG || "https://i.ibb.co/5xzWmSxn/20250224-094453.png",
 ALIVE_MSG : process.env.ALIVE_MSG || "*🤖𝐇𝐞𝐲",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", // true,false
